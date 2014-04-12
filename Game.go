9ea64af84607fca063265cdfe1fb8ec565c895a7
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2014-04-12 13:39:08 -0300

    Restructure some functions into helper

--- Game.go
+++ Game.go
@@ -2,46 +2,7 @@ package main
 
 import (
 	sf "bitbucket.org/krepa098/gosfml2"
-	"encoding/json"
-	"os"
-	"strconv"
-	"strings"
 )
-
-type Settings struct {
-	Resolution string `json:"resolution"`
-	SpriteSize int
-	resW       uint
-	resH       uint
-}
-
-func ReadSettings() Settings {
-	file, err := os.Open("conf.json")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	jParser := json.NewDecoder(file)
-	var s Settings
-	if err = jParser.Decode(&s); err != nil {
-		panic(err)
-	}
-	rs := strings.Split(s.Resolution, "x")
-	resW, err := strconv.Atoi(rs[0])
-	s.resW = uint(resW)
-	if err != nil {
-		panic(s)
-	}
-
-	resH, err := strconv.Atoi(rs[1])
-	s.resH = uint(resH)
-	if err != nil {
-		panic(err)
-	}
-
-	return s
-}
 
 type Drawer interface {
 	Draw(*sf.RenderWindow)
--- helper.go
+++ helper.go
@@ -4,6 +4,8 @@ import (
 	sf "bitbucket.org/krepa098/gosfml2"
 	"encoding/json"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type Spriter interface {
@@ -33,4 +35,39 @@ func ReadJson(folder, name string) map[string]interface{} {
 	data := t.(map[string]interface{})
 
 	return data
+}
+
+type Settings struct {
+	Resolution string `json:"resolution"`
+	SpriteSize int
+	resW       uint
+	resH       uint
+}
+
+func ReadSettings() Settings {
+	file, err := os.Open("conf.json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	jParser := json.NewDecoder(file)
+	var s Settings
+	if err = jParser.Decode(&s); err != nil {
+		panic(err)
+	}
+	rs := strings.Split(s.Resolution, "x")
+	resW, err := strconv.Atoi(rs[0])
+	s.resW = uint(resW)
+	if err != nil {
+		panic(s)
+	}
+
+	resH, err := strconv.Atoi(rs[1])
+	s.resH = uint(resH)
+	if err != nil {
+		panic(err)
+	}
+
+	return s
 }
