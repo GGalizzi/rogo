399fafdfdc9b701997f9da4d0b770502fd33f3e6
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2014-04-15 00:52:10 -0300

    Delete contents of log.txt on open, and create it if necessary.

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
-import "runtime"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
 func init() {
 	runtime.LockOSThread()
 
-	/*
-		f, err := os.OpenFile("log.txt", os.O_WRONLY, 0660)
-		if err != nil {
-			fmt.Println("Can't open log.txt -- ", err)
-		}
+	f, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE, 0660)
+	if err != nil {
+		fmt.Println("Can't open log.txt -- ", err)
+	}
 
-		err = f.Truncate(0)
-		if err != nil {
-			fmt.Println("Can't truncate log.txt -- ", err)
-		}*/
+	err = f.Truncate(0)
+	if err != nil {
+		fmt.Println("Can't truncate log.txt -- ", err)
+	}
 
 }
 
